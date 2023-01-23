578ca5fe7a666f835c2496bb50513550f49c0203
Tim O'Neal <123408733+wacommunityalliance@users.noreply.github.com>
2023-01-23 13:40:15 -0800

    Update map.js

--- map.js
+++ map.js
@@ -68,169 +68,169 @@ map.on('load', function () {
         'wa_counties_outlines'
     );
 
-//     map.addLayer(
-//         {
-//             id: 'wa_counties_g2022_SoS',
-//             type: 'fill',
-//             source: {
-//                 type: 'geojson',
-//                 data: 'countyResults.geojson'
-//             },
-//             layout: {
-//                 'visibility': 'none'
-//             },
-//             paint: {
-//                 'fill-color': [
-//                     'match', 
-//                     ['get', 'county_g2022_SoS_Winner'],
-//                     'Democrat', '#6193c7',
-//                     'Republican', '#cf635d',
-//                     'Independent', '#fdb614',
-//                     'Other', '#88939b',
-//                     '#ffffff'
-//                 ],
-//                 'fill-outline-color': '#ffffff',
-//                 'fill-opacity': [
-//                     'step',
-//                     ['get', 'county_g2022_SoS_Winner_pct'],
-//                     0.5, 50,
-//                     0.6, 60,
-//                     0.7, 70,
-//                     0.8, 80,
-//                     0.9, 90,
-//                     1.0, 100,
-//                     0.1
-//                 ]
-//             },
-//             maxzoom: 7
-//         },
-//         'wa_counties_g2022_FSen'
-//     );
+    map.addLayer(
+        {
+            id: 'wa_counties_g2022_SoS',
+            type: 'fill',
+            source: {
+                type: 'geojson',
+                data: 'countyResults.geojson'
+            },
+            layout: {
+                'visibility': 'none'
+            },
+            paint: {
+                'fill-color': [
+                    'match', 
+                    ['get', 'county_g2022_SoS_Winner'],
+                    'Democrat', '#6193c7',
+                    'Republican', '#cf635d',
+                    'Independent', '#fdb614',
+                    'Other', '#88939b',
+                    '#ffffff'
+                ],
+                'fill-outline-color': '#ffffff',
+                'fill-opacity': [
+                    'step',
+                    ['get', 'county_g2022_SoS_Winner_pct'],
+                    0.5, 50,
+                    0.6, 60,
+                    0.7, 70,
+                    0.8, 80,
+                    0.9, 90,
+                    1.0, 100,
+                    0.1
+                ]
+            },
+            maxzoom: 7
+        },
+        'wa_counties_g2022_FSen'
+    );
     
-//     map.addLayer(
-//         {
-//             id: 'wa_precincts_outlines',
-//             type: 'line',
-//             source: {
-//                 type: 'geojson',
-//                 data: 'precinctResults.geojson',
-//             },
-//             paint: {
-//                 'line-color': '#ffffff',
-//                 'line-width': 0.0,
-//             },
-//             minzoom: 7
-//         },
-//         'wa_counties_g2022_FSen'
-//     );
+    map.addLayer(
+        {
+            id: 'wa_precincts_outlines',
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: 'precinctResults.geojson',
+            },
+            paint: {
+                'line-color': '#ffffff',
+                'line-width': 0.0,
+            },
+            minzoom: 7
+        },
+        'wa_counties_g2022_FSen'
+    );
 
-//     map.addLayer(
-//         {
-//             id: 'wa_precincts_g2022_FSen',
-//             type: 'fill',
-//             source: {
-//                 type: 'geojson',
-//                 data: 'precinctResults.geojson',
-//             },
-//             layout: {
-//                 'visibility': 'none'
-//             },
-//             paint: {
-//                 'fill-color': [
-//                     'match', 
-//                     ['get', 'precinct_g2022_FSen_Winner'],
-//                     'Democrat', '#6193c7',
-//                     'Republican', '#cf635d',
-//                     'Independent', '#fdb614',
-//                     'Other', '#88939b',
-//                     '#c7c7c7'
-//                 ],
-//                 'fill-outline-color': '#ffffff',
-//                 'fill-opacity': [
-//                     'step',
-//                     ['get', 'precinct_g2022_FSen_Winner_pct'],
-//                     0.5, 50,
-//                     0.6, 60,
-//                     0.7, 70,
-//                     0.8, 80,
-//                     0.9, 90,
-//                     1.0, 100,
-//                     0.1
-//                 ]
-//             },
-//             minzoom: 7
-//         },
-//         'wa_precincts_outlines'
-//     );
+    map.addLayer(
+        {
+            id: 'wa_precincts_g2022_FSen',
+            type: 'fill',
+            source: {
+                type: 'geojson',
+                data: 'precinctResults.geojson',
+            },
+            layout: {
+                'visibility': 'none'
+            },
+            paint: {
+                'fill-color': [
+                    'match', 
+                    ['get', 'precinct_g2022_FSen_Winner'],
+                    'Democrat', '#6193c7',
+                    'Republican', '#cf635d',
+                    'Independent', '#fdb614',
+                    'Other', '#88939b',
+                    '#c7c7c7'
+                ],
+                'fill-outline-color': '#ffffff',
+                'fill-opacity': [
+                    'step',
+                    ['get', 'precinct_g2022_FSen_Winner_pct'],
+                    0.5, 50,
+                    0.6, 60,
+                    0.7, 70,
+                    0.8, 80,
+                    0.9, 90,
+                    1.0, 100,
+                    0.1
+                ]
+            },
+            minzoom: 7
+        },
+        'wa_precincts_outlines'
+    );
 
-//     map.addLayer(
-//         {
-//             id: 'wa_precincts_g2022_SoS',
-//             type: 'fill',
-//             source: {
-//                 type: 'geojson',
-//                 data: 'precinctResults.geojson',
-//             },
-//             layout: {
-//                 'visibility': 'none'
-//             },
-//             paint: {
-//                 'fill-color': [
-//                     'match', 
-//                     ['get', 'precinct_g2022_SoS_Winner'],
-//                     'Democrat', '#6193c7',
-//                     'Republican', '#cf635d',
-//                     'Independent', '#fdb614',
-//                     'Other', '#88939b',
-//                     '#c7c7c7'
-//                 ],
-//                 'fill-outline-color': '#ffffff',
-//                 'fill-opacity': [
-//                     'step',
-//                     ['get', 'precinct_g2022_SoS_Winner_pct'],
-//                     0.5, 50,
-//                     0.6, 60,
-//                     0.7, 70,
-//                     0.8, 80,
-//                     0.9, 90,
-//                     1.0, 100,
-//                     0.1
-//                 ]
-//             },
-//             minzoom: 7
-//         },
-//         'wa_precincts_g2022_FSen'
-//     );
+    map.addLayer(
+        {
+            id: 'wa_precincts_g2022_SoS',
+            type: 'fill',
+            source: {
+                type: 'geojson',
+                data: 'precinctResults.geojson',
+            },
+            layout: {
+                'visibility': 'none'
+            },
+            paint: {
+                'fill-color': [
+                    'match', 
+                    ['get', 'precinct_g2022_SoS_Winner'],
+                    'Democrat', '#6193c7',
+                    'Republican', '#cf635d',
+                    'Independent', '#fdb614',
+                    'Other', '#88939b',
+                    '#c7c7c7'
+                ],
+                'fill-outline-color': '#ffffff',
+                'fill-opacity': [
+                    'step',
+                    ['get', 'precinct_g2022_SoS_Winner_pct'],
+                    0.5, 50,
+                    0.6, 60,
+                    0.7, 70,
+                    0.8, 80,
+                    0.9, 90,
+                    1.0, 100,
+                    0.1
+                ]
+            },
+            minzoom: 7
+        },
+        'wa_precincts_g2022_FSen'
+    );
 });
 
-// toggleLayer(['wa_counties_g2022_FSen', 'wa_precincts_g2022_FSen'], 'U.S. Senate');
-// toggleLayer(['wa_counties_g2022_SoS', 'wa_precincts_g2022_SoS'], 'Secretary of State')
+toggleLayer(['wa_counties_g2022_FSen', 'wa_precincts_g2022_FSen'], 'U.S. Senate');
+toggleLayer(['wa_counties_g2022_SoS', 'wa_precincts_g2022_SoS'], 'Secretary of State')
 
-// function toggleLayer(ids, name) {
-//     var link = document.createElement('a');
-//     link.href = '#';
-//     link.className = 'active';
-//     link.textContent = name;
+function toggleLayer(ids, name) {
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = name;
 
-//     link.onclick = function (e) {
-//         e.preventDefault();
-//         e.stopPropagation();
-//         for (layers in ids) {
-//             var visibility = map.getLayoutProperty(ids[layers], 'visibility');
-//             if (visibility === 'visible') {
-//                 map.setLayoutProperty(ids[layers], 'visibility', 'none');
-//                 this.className = '';
-//             }
-//             else {
-//                 this.className = 'active';
-//                 map.setLayoutProperty(ids[layers], 'visibility', 'visible');
-//             }
-//         }
-//     };
+    link.onclick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        for (layers in ids) {
+            var visibility = map.getLayoutProperty(ids[layers], 'visibility');
+            if (visibility === 'visible') {
+                map.setLayoutProperty(ids[layers], 'visibility', 'none');
+                this.className = '';
+            }
+            else {
+                this.className = 'active';
+                map.setLayoutProperty(ids[layers], 'visibility', 'visible');
+            }
+        }
+    };
 
-//     var layers = document.getElementById('menu');
-//     layers.appendChild(link);
-// }
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
 
 
 
