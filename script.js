34b317516a63aabe861cb0b5ad259c9659e4f590
Tim O'Neal <tim@wacommunityalliance.org>
2023-06-05 15:43:26 -0700

    restructured UI for data selection

--- script.js
+++ script.js
@@ -24,6 +24,20 @@ const map = new mapboxgl.Map({
 map.on('click', 'selectedLayer', (e) => {
     _log(e);
 
+    // var features = e.features[0];
+    // var coordinates = e.lngLat;
+
+    // var popupContent = '<table>';
+    // popupContent += '<tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr>';
+
+    // for (var i = 0; i < features.properties.data.length; i++) {
+    //     var rowData = features.properties.data[i];
+    //     popupContent += '<tr><td>' + rowData.column1 + '</td><td>' + rowData.column2 + '</td><td>' + rowData.column3 + '</td></tr>';
+    //   }
+
+    // popupContent += '</table>';
+
+
     const label = e.features[0].properties.Title.toUpperCase();
     const totalVotes = e.features[0].properties.Votes_total;
 
@@ -51,83 +65,83 @@ map.on('click', 'selectedLayer', (e) => {
     const party5 = e.features[0].properties.Party_5;
     const votes5 = e.features[0].properties.Votes_5;
     const pct5 = Math.round(e.features[0].properties.Pct_5 * 100) / 100;
-    
+
     let message;
 
     if (candidate1 == null) {
-        message = 
-        '<h3>' + label + '</h3>'
-        + '<p>No data available</p>'
+        message =
+            '<h2>' + label + '</h2>'
+            + '<p>No data available</p>'
     }
 
     else if (votes2 == 0) {
-        message = 
-        '<h3>' + label + '</h3>'
-        + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-        + '<p>Total votes: ' + totalVotes + '</p>' 
+        message =
+            '<h2>' + label + '</h2>'
+            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+            + '<p>Total votes: ' + totalVotes + '</p>'
     }
 
     else if (votes3 == 0) {
         if (candidate2 == 'Write-In') {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ': ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ': ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
         else {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
     }
 
     else if (votes4 == 0) {
         if (candidate3 == 'Write-In') {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>' + candidate3 + ': ' + votes3.charAt(0) + ' votes (' + pct3 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>' + candidate3 + ': ' + votes3.charAt(0) + ' votes (' + pct3 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
         else {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
     }
 
     else if (votes4 != 0) {
         if (candidate4 == 'Write-In') {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
-            + '<p>' + candidate4 + ': ' + votes4.charAt(0) + ' votes (' + pct4 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
+                + '<p>' + candidate4 + ': ' + votes4.charAt(0) + ' votes (' + pct4 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
         else {
-            message = 
-            '<h3>' + label + '</h3>'
-            + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
-            + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
-            + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
-            + '<p>' + candidate4 + ' (' + party4.charAt(0) + '): ' + votes4 + ' votes (' + pct4 + '%)<br></p'
-            + '<p>Total votes: ' + totalVotes + '</p>' 
+            message =
+                '<h2>' + label + '</h2>'
+                + '<p>' + candidate1 + ' (' + party1.charAt(0) + '): ' + votes1 + ' votes (' + pct1 + '%)<br></p'
+                + '<p>' + candidate2 + ' (' + party2.charAt(0) + '): ' + votes2 + ' votes (' + pct2 + '%)<br></p'
+                + '<p>' + candidate3 + ' (' + party3.charAt(0) + '): ' + votes3 + ' votes (' + pct3 + '%)<br></p'
+                + '<p>' + candidate4 + ' (' + party4.charAt(0) + '): ' + votes4 + ' votes (' + pct4 + '%)<br></p'
+                + '<p>Total votes: ' + totalVotes + '</p>'
         }
     }
 
     else {
-        message = 
-        + '<p>hmm</p>' 
-    }   
+        message =
+            + '<p>hmm</p>'
+    }
 
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
@@ -135,49 +149,152 @@ map.on('click', 'selectedLayer', (e) => {
         .addTo(map)
 });
 
-// Convert the tree data to jstree format
-function convertToJsTreeFormat(treeData) {
+
+
+// // Convert the tree data to jstree format
+// function convertToJsTreeFormat(treeData) {
+//     if (!treeData || !treeData.items) return [];
+
+//     return treeData.items.map(item => {
+//         const children = convertToJsTreeFormat(item);
+//         return {
+//             text: item.title,
+//             data: {
+//                 path: item.path
+//             },
+//             children
+//         };
+//     });
+// }
+
+
+// fetch('Data/data.json')
+//     .then(response => response.json())
+//     .then(treeData => {
+//         _log(treeData);
+
+//         // If you don't want a tree, use treeData to populate slider, buttons, etc
+//         // Initialize the jstree
+//         $('#tree').jstree({
+//             core: {
+//                 data: convertToJsTreeFormat(treeData)
+//             }
+//         });
+
+//         // if you don't want a tree, replace this click handling with state mgmt + click handling for slider, buttons, etc
+//         // jstree event handlers
+//         $('#tree').on('select_node.jstree', function (e, data) {
+//             const node = data.node;
+//             const hasChildren = node.children && node.children.length > 0;
+
+//             if (hasChildren) {
+//                 $('#tree').jstree('toggle_node', node);
+//             } else {
+//                 layerSelected(node.data.path);
+//             }
+//         });
+//     });
+
+
+// Convert the tree data to dropdown menu format
+function convertToDropdownFormat(treeData) {
     if (!treeData || !treeData.items) return [];
 
     return treeData.items.map(item => {
-        const children = convertToJsTreeFormat(item);
+        const children = convertToDropdownFormat(item);
         return {
-            text: item.title,
-            data: {
-                path: item.path
-            },
+            value: item.title,
             children
         };
     });
 }
 
+let selectedFirstValue = '';
+let selectedSecondValue = '';
 
 fetch('Data/data.json')
     .then(response => response.json())
     .then(treeData => {
-        _log(treeData);
+        // Convert treeData to dropdown format
+        const dropdownData = convertToDropdownFormat(treeData);
 
-        // If you don't want a tree, use treeData to populate slider, buttons, etc
-        // Initialize the jstree
-        $('#tree').jstree({
-            core: {
-                data: convertToJsTreeFormat(treeData)
+        // Populate the first dropdown menu
+        const firstDropdown = document.getElementById('first-dropdown');
+        const firstDefaultOption = document.createElement('option');
+        firstDefaultOption.value = '';
+        firstDefaultOption.textContent = 'Select year';
+        firstDropdown.appendChild(firstDefaultOption);
+
+        dropdownData.forEach(item => {
+            const option = document.createElement('option');
+            option.value = item.value;
+            option.textContent = item.value;
+            firstDropdown.appendChild(option);
+        });
+
+        // Update the second dropdown menu when the first dropdown selection changes
+        firstDropdown.addEventListener('change', () => {
+            const secondDropdown = document.getElementById('second-dropdown');
+            selectedFirstValue = firstDropdown.value; // Update selectedFirstValue
+            const selectedFirstItem = dropdownData.find(item => item.value === selectedFirstValue);
+            secondDropdown.innerHTML = ''; // Clear the previous options
+
+            const secondDefaultOption = document.createElement('option');
+            secondDefaultOption.value = '';
+            secondDefaultOption.textContent = 'Select position';
+            secondDropdown.appendChild(secondDefaultOption);
+
+            if (selectedFirstItem && selectedFirstItem.children) {
+                selectedFirstItem.children.forEach(item => {
+                    const option = document.createElement('option');
+                    option.value = item.value;
+                    option.textContent = item.value;
+                    secondDropdown.appendChild(option);
+                });
             }
         });
 
-        // if you don't want a tree, replace this click handling with state mgmt + click handling for slider, buttons, etc
-        // jstree event handlers
-        $('#tree').on('select_node.jstree', function (e, data) {
-            const node = data.node;
-            const hasChildren = node.children && node.children.length > 0;
+        // Update the third dropdown menu when the second dropdown selection changes
+        const secondDropdown = document.getElementById('second-dropdown');
+        secondDropdown.addEventListener('change', () => {
+            const thirdDropdown = document.getElementById('third-dropdown');
+            selectedSecondValue = secondDropdown.value; // Update selectedSecondValue
+            const selectedFirstItem = dropdownData.find(item => item.value === selectedFirstValue);
+            const selectedSecondItem = selectedFirstItem && selectedFirstItem.children
+                ? selectedFirstItem.children.find(item => item.value === selectedSecondValue)
+                : null;
+            thirdDropdown.innerHTML = ''; // Clear the previous options
 
-            if (hasChildren) {
-                $('#tree').jstree('toggle_node', node);
-            } else {
-                layerSelected(node.data.path);
+            const thirdDefaultOption = document.createElement('option');
+            thirdDefaultOption.value = '';
+            thirdDefaultOption.textContent = 'Select layer';
+            thirdDropdown.appendChild(thirdDefaultOption);
+
+            if (selectedSecondItem && selectedSecondItem.children) {
+                selectedSecondItem.children.forEach(item => {
+                    const option = document.createElement('option');
+                    option.value = item.value;
+                    option.textContent = item.value;
+                    thirdDropdown.appendChild(option);
+                });
             }
+        });
+
+        // Call the layerSelected function when the third dropdown selection changes
+        const thirdDropdown = document.getElementById('third-dropdown');
+        thirdDropdown.addEventListener('change', () => {
+            const selectedThirdValue = thirdDropdown.value;
+
+            // Generate the selected path
+            const path = selectedFirstValue + '/' + selectedSecondValue + '/' + selectedThirdValue;
+
+            // Call the layerSelected function with the selected path
+            layerSelected(path);
         });
     });
+
+
+
 
 // Function to load and display the GeoJSON file
 // this is agnostic, as long as it receives the absolute path to the GeoJSON file,
@@ -195,14 +312,16 @@ function layerSelected(path) {
     // Find the index of the first symbol layer in the map style.
     let firstSymbolId;
     for (const layer of layers) {
-    if (layer.type === 'symbol') {
-    firstSymbolId = layer.id;
-    break;
-    }};
+        if (layer.type === 'symbol') {
+            firstSymbolId = layer.id;
+            break;
+        }
+    };
 
     map.addSource('selectedLayer', {
         type: 'geojson',
-        data: '/ElectionMapsWA' + path
+        // data: '/ElectionMapsWA' + path   // uncomment to publish
+        data: '/Data/' + path + '.geojson'                          // uncomment to debug
     });
 
     map.addLayer({
@@ -223,29 +342,29 @@ function layerSelected(path) {
         layout: {},
         paint: {
             'fill-color': [         // fill polygon based on 1st place candidate's party
-                    'match',
-                    ['get', 'Party_1'],
-                    'Democratic', '#4f93ba',    
-                    'Republican', '#cf635d',   
-                    'Independent', '#fac566',   
-                    'Democratic2', '#868fba',   
-                    'Republican2', '#f68f3e',   
-                    'Independent2', '#119f92',  
-                    'transparent'
-                ],
-                'fill-opacity': [       // set fill opacity to margin b/w 1st and 2nd place candidates
-                    'step',
-                    ['get', 'Margin'],
-                    0.15, 5,
-                    0.3, 10,
-                    0.45, 15,
-                    0.6, 20,
-                    0.75, 101,
-                    0
-                ]
+                'match',
+                ['get', 'Party_1'],
+                'Democratic', '#4f93ba',
+                'Republican', '#cf635d',
+                'Independent', '#fac566',
+                'Democratic2', '#868fba',
+                'Republican2', '#f68f3e',
+                'Independent2', '#119f92',
+                'transparent'
+            ],
+            'fill-opacity': [       // set fill opacity to margin b/w 1st and 2nd place candidates
+                'step',
+                ['get', 'Margin'],
+                0.15, 5,
+                0.3, 10,
+                0.45, 15,
+                0.6, 20,
+                0.75, 101,
+                0
+            ]
         }
     },
-    firstSymbolId
+        firstSymbolId
     );
 };
 
