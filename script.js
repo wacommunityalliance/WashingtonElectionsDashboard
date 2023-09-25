afcc3c4bb2da9c14b723b821d3bb76f3c1caa4a4
Tim O'Neal <tim@wacommunityalliance.org>
2023-09-25 09:29:49 -0700

    added mailto @BenAnderstone

--- script.js
+++ script.js
@@ -260,8 +260,8 @@ function layerSelected(path) {
 
     map.addSource('selectedLayer', {
         type: 'geojson',
-        data: '/WashingtonElectionsDashboard/Data/' + path + '.geojson'  // uncomment to publish
-        // data: '/Data/' + path + '.geojson'               // uncomment to debug
+        // data: '/WashingtonElectionsDashboard/Data/' + path + '.geojson'  // uncomment to publish
+        data: '/Data/' + path + '.geojson'               // uncomment to debug
     });
 
     map.addLayer({
